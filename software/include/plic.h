2e4778961eb869363c6ac4c2f766c0b5483f4ae4
Daniel Schultz <dnltz@aesc-silicon.de>
2025-02-17 18:43:12 +0100

    software: plic: Add IRQ Claim
    Add a new function to claim an interrupt inside the PLIC.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- software/include/plic.h
+++ software/include/plic.h
@@ -5,11 +5,13 @@ struct plic_driver {
 	unsigned int *gateway_priority;
 	unsigned int *gateway_pending;
 	unsigned int *target_enable;
+	unsigned int *claim;
 };
 
 
 int plic_init(struct plic_driver *driver, unsigned int base_address);
 int plic_irq_enable(struct plic_driver *driver, unsigned int number);
 int plic_irq_disable(struct plic_driver *driver, unsigned int number);
+int plic_irq_claim(struct plic_driver *driver, unsigned int number);
 
 #endif

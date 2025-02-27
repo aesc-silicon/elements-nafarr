e0a44266d8974392dd77308bba99de0a53d2abf2
Daniel Schultz <dnltz@aesc-silicon.de>
2025-02-27 16:35:09 +0100

    software: driver: gpio: Add interrupt functions
    Add functions to use the interrupt controller in the GPIO controller.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- software/driver/gpio.c
+++ software/driver/gpio.c
@@ -61,3 +61,59 @@ void gpio_dir_clr(struct gpio_driver *driver, unsigned int pin)
 	gpio->dir_en = val;
 }
 
+int gpio_irq_enable(struct gpio_driver *driver, unsigned int pin, int flags)
+{
+	volatile struct gpio_regs *gpio = driver->regs;
+
+	if (flags & GPIO_IRQ_LEVEL_HIGH) {
+		gpio->irq_high_pending |= 1 << pin;
+		gpio->irq_high_mask |= 1 << pin;
+	}
+	if (flags & GPIO_IRQ_LEVEL_LOW) {
+		gpio->irq_low_pending |= 1 << pin;
+		gpio->irq_low_mask |= 1 << pin;
+	}
+	if (flags & GPIO_IRQ_RISING_EDGE) {
+		gpio->irq_rising_pending |= 1 << pin;
+		gpio->irq_rising_mask |= 1 << pin;
+	}
+	if (flags & GPIO_IRQ_FALLING_EDGE) {
+		gpio->irq_falling_pending |= 1 << pin;
+		gpio->irq_falling_mask |= 1 << pin;
+	}
+
+	return 1;
+}
+
+int gpio_irq_disable(struct gpio_driver *driver, unsigned int pin, int flags)
+{
+	volatile struct gpio_regs *gpio = driver->regs;
+
+	if (flags & GPIO_IRQ_LEVEL_HIGH)
+		gpio->irq_high_mask &= ~(1 << pin);
+	if (flags & GPIO_IRQ_LEVEL_LOW)
+		gpio->irq_low_mask &= ~(1 << pin);
+	if (flags & GPIO_IRQ_RISING_EDGE)
+		gpio->irq_rising_mask &= ~(1 << pin);
+	if (flags & GPIO_IRQ_FALLING_EDGE)
+		gpio->irq_falling_mask &= ~(1 << pin);
+
+	return 1;
+}
+
+int gpio_irq_ready(struct gpio_driver *driver, unsigned int pin, int flags)
+{
+	volatile struct gpio_regs *gpio = driver->regs;
+	int ret = 0;
+
+	if (flags & GPIO_IRQ_LEVEL_HIGH)
+		ret |= !!(gpio->irq_high_pending & (1 << pin));
+	if (flags & GPIO_IRQ_LEVEL_LOW)
+		ret |= !!(gpio->irq_low_pending & (1 << pin));
+	if (flags & GPIO_IRQ_RISING_EDGE)
+		ret |= !!(gpio->irq_rising_pending & (1 << pin));
+	if (flags & GPIO_IRQ_FALLING_EDGE)
+		ret |= !!(gpio->irq_falling_pending & (1 << pin));
+
+	return ret;
+}
--- software/include/gpio.h
+++ software/include/gpio.h
@@ -5,26 +5,34 @@ struct gpio_regs {
 	int data_in;
 	int data_out;
 	int dir_en;
-	int irq_high_mask;
+	int reserved;
 	int irq_high_pending;
-	int irq_low_mask;
+	int irq_high_mask;
 	int irq_low_pending;
-	int irq_falling_mask;
-	int irq_falling_pending;
-	int irq_rising_mask;
+	int irq_low_mask;
 	int irq_rising_pending;
+	int irq_rising_mask;
+	int irq_falling_pending;
+	int irq_falling_mask;
 };
 
 struct gpio_driver {
 	volatile struct gpio_regs *regs;
 };
 
+#define GPIO_IRQ_LEVEL_HIGH		(1 << 0)
+#define GPIO_IRQ_LEVEL_LOW		(1 << 1)
+#define GPIO_IRQ_RISING_EDGE		(1 << 2)
+#define GPIO_IRQ_FALLING_EDGE		(1 << 3)
+
 int gpio_init(struct gpio_driver *driver, unsigned int base_address);
 unsigned int gpio_value_get(struct gpio_driver *driver, unsigned int pin);
 void gpio_value_set(struct gpio_driver *driver, unsigned int pin);
 void gpio_value_clr(struct gpio_driver *driver, unsigned int pin);
-
 void gpio_dir_set(struct gpio_driver *driver, unsigned int pin);
 void gpio_dir_clr(struct gpio_driver *driver, unsigned int pin);
+int gpio_irq_enable(struct gpio_driver *driver, unsigned int pin, int flags);
+int gpio_irq_disable(struct gpio_driver *driver, unsigned int pin, int flags);
+int gpio_irq_ready(struct gpio_driver *driver, unsigned int pin, int flags);
 
 #endif

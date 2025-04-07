1ef0b22a7d54419a2a4d7c3eb6be9b269e4bd4f2
Daniel Schultz <dnltz@aesc-silicon.de>
2025-04-07 16:40:57 +0200

    hardware: nafarr: peripherals Add Pinmux Controller
    This IP allows to mux between multiple input sources to one
    physical output pin.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

---
+++ hardware/scala/nafarr/peripherals/pinmux/Pinmux.scala
@@ -0,0 +1,83 @@
+package nafarr.peripherals.pinmux
+
+import spinal.core._
+import spinal.lib._
+import spinal.lib.bus.misc._
+import spinal.lib.bus.amba3.apb._
+import spinal.lib.bus.avalon._
+import spinal.lib.bus.wishbone._
+import spinal.lib.io.{TriStateArray, TriState}
+
+import scala.collection.mutable.ArrayBuffer
+
+object Pinmux {
+  case class Parameter(width: Int) {
+    require(width > 0, "At least one pin is required.")
+  }
+
+  case class Io(p: Parameter) extends Bundle {
+    val pins = master(TriStateArray(p.width bits))
+  }
+
+  class Core[T <: spinal.core.Data with IMasterSlave](
+      p: PinmuxCtrl.Parameter,
+      mapping: ArrayBuffer[(Int, List[Int])],
+      busType: HardType[T],
+      factory: T => BusSlaveFactory
+  ) extends Component {
+    val io = new Bundle {
+      val bus = slave(busType())
+      val pins = Io(p.io)
+      val inputs = slave(TriStateArray(p.inputs bits))
+    }
+    val ctrl = PinmuxCtrl(p, mapping)
+    ctrl.io.pins <> io.pins.pins
+    ctrl.io.inputs <> io.inputs
+    val mapper = PinmuxCtrl.Mapper(factory(io.bus), ctrl.io, p)
+
+    def headerBareMetal(
+        name: String,
+        address: BigInt,
+        size: BigInt,
+        irqNumber: Option[Int] = null
+    ) = {
+      val baseAddress = "%08x".format(address.toInt)
+      val regSize = "%04x".format(size.toInt)
+      var dt = s"""#define ${name.toUpperCase}_BASE\t\t0x${baseAddress}\n"""
+      dt
+    }
+  }
+}
+
+case class Apb3Pinmux(
+    parameter: PinmuxCtrl.Parameter,
+    mapping: ArrayBuffer[(Int, List[Int])],
+    busConfig: Apb3Config = Apb3Config(12, 32)
+) extends Pinmux.Core[Apb3](
+      parameter,
+      mapping,
+      Apb3(busConfig),
+      Apb3SlaveFactory(_)
+    ) { val dummy = 0 }
+
+case class WishbonePinmux(
+    parameter: PinmuxCtrl.Parameter,
+    mapping: ArrayBuffer[(Int, List[Int])],
+    busConfig: WishboneConfig = WishboneConfig(12, 32)
+) extends Pinmux.Core[Wishbone](
+      parameter,
+      mapping,
+      Wishbone(busConfig),
+      WishboneSlaveFactory(_)
+    ) { val dummy = 0 }
+
+case class AvalonMMPinmux(
+    parameter: PinmuxCtrl.Parameter,
+    mapping: ArrayBuffer[(Int, List[Int])],
+    busConfig: AvalonMMConfig = AvalonMMConfig.fixed(12, 32, 1)
+) extends Pinmux.Core[AvalonMM](
+      parameter,
+      mapping,
+      AvalonMM(busConfig),
+      AvalonMMSlaveFactory(_)
+    ) { val dummy = 0 }

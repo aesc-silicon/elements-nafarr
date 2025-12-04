56bb5f30209b839fd82ff1c0280071feea2abed8
Daniel Schultz <dnltz@aesc-silicon.de>
2025-12-04 10:05:15 +0100

    hardware: nafarr: blackbox: sg13g2: Make Memory more generic
    Make the memory blackbox component more generic by passing only
    the width and total size. The Memory object will pick the matching
    IHP sram block.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- hardware/scala/nafarr/blackboxes/ihp/sg13g2/Memory.scala
+++ hardware/scala/nafarr/blackboxes/ihp/sg13g2/Memory.scala
@@ -3,11 +3,13 @@ package nafarr.blackboxes.ihp.sg13g2
 import spinal.core._
 
 object Memory {
+  def apply(width: Int, size: Int): IHPSG13Memory = (width, size) match {
+    case (8, 1024) => RM_IHPSG13_1P_1024x8_c2_bm_bist()
+    case (32, 4096) => RM_IHPSG13_1P_512x32_c2_bm_bist()
+    case _ => throw new IllegalArgumentException(s"Unsupported width and size: $width x $size")
+  }
 
-  case class RM_IHPSG13_1P_512x32_c2_bm_bist() extends BlackBox {
-    val addrWidth = 9
-    val dataWidth = 32
-
+  abstract class IHPSG13Memory(val addrWidth: Int, val dataWidth: Int) extends BlackBox {
     val A_CLK = in(Bool())
     val A_MEN = in(Bool())
     val A_WEN = in(Bool())
@@ -41,7 +43,10 @@ object Memory {
 
     mapCurrentClockDomain(A_CLK)
     mapCurrentClockDomain(A_BIST_CLK)
+  }
 
+  case class RM_IHPSG13_1P_512x32_c2_bm_bist()
+      extends IHPSG13Memory(addrWidth = 9, dataWidth = 32) {
     addRTLPath(
       System.getenv(
         "NAFARR_BASE"
@@ -54,44 +59,8 @@ object Memory {
     )
   }
 
-  case class RM_IHPSG13_1P_1024x8_c2_bm_bist() extends BlackBox {
-    val addrWidth = 10
-    val dataWidth = 8
-
-    val A_CLK = in(Bool())
-    val A_MEN = in(Bool())
-    val A_WEN = in(Bool())
-    val A_REN = in(Bool())
-    val A_ADDR = in(Bits(addrWidth bits))
-    val A_DIN = in(Bits(dataWidth bits))
-    val A_DLY = in(Bool())
-    val A_DOUT = out(Bits(dataWidth bits))
-    val A_BM = in(Bits(dataWidth bits))
-
-    val A_BIST_CLK = in(Bool())
-    val A_BIST_EN = in(Bool())
-    val A_BIST_MEN = in(Bool())
-    val A_BIST_WEN = in(Bool())
-    val A_BIST_REN = in(Bool())
-    val A_BIST_ADDR = in(Bits(addrWidth bits))
-    val A_BIST_DIN = in(Bits(dataWidth bits))
-    val A_BIST_BM = in(Bits(dataWidth bits))
-
-    def connectDefaults() {
-      this.A_DLY := True
-
-      this.A_BIST_EN := False
-      this.A_BIST_MEN := False
-      this.A_BIST_WEN := False
-      this.A_BIST_REN := False
-      this.A_BIST_ADDR := 0
-      this.A_BIST_DIN := 0
-      this.A_BIST_BM := 0
-    }
-
-    mapCurrentClockDomain(A_CLK)
-    mapCurrentClockDomain(A_BIST_CLK)
-
+  case class RM_IHPSG13_1P_1024x8_c2_bm_bist()
+      extends IHPSG13Memory(addrWidth = 10, dataWidth = 8) {
     addRTLPath(
       System.getenv(
         "NAFARR_BASE"

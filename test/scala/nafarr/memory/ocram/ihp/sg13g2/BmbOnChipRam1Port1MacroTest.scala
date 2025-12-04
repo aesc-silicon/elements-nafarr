b8a55d881106e36923ab8fc11b03d87bebbe19f6
Daniel Schultz <dnltz@aesc-silicon.de>
2025-12-04 10:14:36 +0100

    hardware: nafarr: memory: sg13g2: Add BMB based OCRAM
    Add a OCRAM with IHP's SRAM blocks for the BMB bus. This OCRAM
    comes with a one or four macro configuration.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

---
+++ test/scala/nafarr/memory/ocram/ihp/sg13g2/BmbOnChipRam1Port1MacroTest.scala
@@ -0,0 +1,145 @@
+package nafarr.memory.ocram.ihp.sg13g2
+
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import spinal.sim._
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib.bus.misc.SizeMapping
+import spinal.lib.bus.bmb._
+import spinal.lib.bus.bmb.sim.BmbDriver
+import nafarr.CheckTester._
+
+class BmbOnChipRam1Port1MacroTest extends AnyFunSuite {
+
+  def defaultSignals(dut: BmbIhpOnChipRam.OnePort1Macro) = {
+    dut.clockDomain.forkStimulus(10)
+    dut.io.bus.cmd.valid #= false
+    dut.io.bus.rsp.ready #= true
+    dut.clockDomain.waitSampling(5)
+  }
+
+  def writeWord(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
+    dut.io.bus.cmd.valid #= true
+    dut.io.bus.cmd.address #= address
+    dut.io.bus.cmd.data #= data
+    dut.io.bus.cmd.mask #= 0xF
+    dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.WRITE
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.valid #= false
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+  }
+
+  def writeShort(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
+    dut.io.bus.cmd.valid #= true
+    dut.io.bus.cmd.address #= address
+    dut.io.bus.cmd.data #= data
+    dut.io.bus.cmd.mask #= 0x3
+    dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.WRITE
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.valid #= false
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+  }
+
+  def writeChar(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
+    dut.io.bus.cmd.valid #= true
+    dut.io.bus.cmd.address #= address
+    dut.io.bus.cmd.data #= data
+    dut.io.bus.cmd.mask #= 0x1
+    dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.WRITE
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.valid #= false
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+  }
+
+  def readAndCheckWord(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
+    dut.io.bus.cmd.valid #= true
+    dut.io.bus.cmd.address #= address
+    dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.READ
+    dut.io.bus.cmd.mask #= 0xF
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.valid #= false
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    assert(dut.io.bus.rsp.data.toBigInt == data)
+  }
+
+  def readAndCheckShort(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
+    dut.io.bus.cmd.valid #= true
+    dut.io.bus.cmd.address #= address
+    dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.READ
+    dut.io.bus.cmd.mask #= 0x3
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.valid #= false
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    assert(dut.io.bus.rsp.data.toBigInt == data)
+  }
+
+  def readAndCheckChar(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
+    dut.io.bus.cmd.valid #= true
+    dut.io.bus.cmd.address #= address
+    dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.READ
+    dut.io.bus.cmd.mask #= 0x1
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.valid #= false
+    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    assert(dut.io.bus.rsp.data.toBigInt == data)
+  }
+
+  test("BmbOnChipRam1Port1Macro") {
+    val compiled = SimConfig.withWave.compile {
+      val cd = ClockDomain.current.copy(frequency = FixedFrequency(100 MHz))
+      val area = new ClockingArea(cd) {
+        val dataBusParameter = BmbParameter(
+          addressWidth = 32,
+          dataWidth = 32,
+          lengthWidth = 6,
+          sourceWidth = 4,
+          contextWidth = 4
+        )
+        val dut = BmbIhpOnChipRam.OnePort1Macro(dataBusParameter, 0x1000)
+      }
+      area.dut
+    }
+    compiled.doSim("default signals") { dut =>
+      defaultSignals(dut)
+    }
+
+    compiled.doSim("word") { dut =>
+      defaultSignals(dut)
+
+      writeWord(dut, 0x0000000, 0x00000000)
+      dut.clockDomain.waitSampling(2)
+      writeWord(dut, 0x0000000, 0x44332211)
+      dut.clockDomain.waitSampling(2)
+      readAndCheckWord(dut, 0x00000000, 0x44332211)
+
+      dut.clockDomain.waitSampling(20)
+    }
+
+    compiled.doSim("short") { dut =>
+      defaultSignals(dut)
+
+      writeWord(dut, 0x0000000, 0x00000000)
+      dut.clockDomain.waitSampling(2)
+      writeShort(dut, 0x0000000, 0x22112211)
+      dut.clockDomain.waitSampling(2)
+      readAndCheckShort(dut, 0x00000000, 0x00002211)
+
+      dut.clockDomain.waitSampling(20)
+    }
+
+    compiled.doSim("char") { dut =>
+      defaultSignals(dut)
+
+      writeWord(dut, 0x0000000, 0x00000000)
+      dut.clockDomain.waitSampling(2)
+      writeChar(dut, 0x0000000, 0x11111111)
+      dut.clockDomain.waitSampling(2)
+      readAndCheckChar(dut, 0x00000000, 0x00000011)
+
+      dut.clockDomain.waitSampling(20)
+    }
+
+  }
+}

1f1114f87d2aa2fae73fe6fab6e90c8f1a9ed818
Daniel Schultz <dnltz@aesc-silicon.de>
2025-12-09 09:21:36 +0100

    hardware: nafarr: memory: ocram: sg13g2: Add Burst Mode
    Allow to read in burst mode from the BmbIhpOnChipRam.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- test/scala/nafarr/memory/ocram/ihp/sg13g2/BmbOnChipRam1Port1MacroTest.scala
+++ test/scala/nafarr/memory/ocram/ihp/sg13g2/BmbOnChipRam1Port1MacroTest.scala
@@ -29,10 +29,13 @@ class BmbOnChipRam1Port1MacroTest extends AnyFunSuite {
     dut.io.bus.cmd.address #= address
     dut.io.bus.cmd.data #= data
     dut.io.bus.cmd.mask #= 0xF
+    dut.io.bus.cmd.length #= 4 - 1
     dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.WRITE
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.cmd.ready.toBoolean == true)
     dut.io.bus.cmd.valid #= false
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.rsp.valid.toBoolean == true)
   }
 
   def writeShort(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
@@ -40,10 +43,13 @@ class BmbOnChipRam1Port1MacroTest extends AnyFunSuite {
     dut.io.bus.cmd.address #= address
     dut.io.bus.cmd.data #= data
     dut.io.bus.cmd.mask #= 0x3
+    dut.io.bus.cmd.length #= 2 - 1
     dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.WRITE
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.cmd.ready.toBoolean == true)
     dut.io.bus.cmd.valid #= false
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.rsp.valid.toBoolean == true)
   }
 
   def writeChar(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
@@ -51,10 +57,13 @@ class BmbOnChipRam1Port1MacroTest extends AnyFunSuite {
     dut.io.bus.cmd.address #= address
     dut.io.bus.cmd.data #= data
     dut.io.bus.cmd.mask #= 0x1
+    dut.io.bus.cmd.length #= 1 - 1
     dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.WRITE
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.cmd.ready.toBoolean == true)
     dut.io.bus.cmd.valid #= false
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.rsp.valid.toBoolean == true)
   }
 
   def readAndCheckWord(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
@@ -62,20 +71,44 @@ class BmbOnChipRam1Port1MacroTest extends AnyFunSuite {
     dut.io.bus.cmd.address #= address
     dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.READ
     dut.io.bus.cmd.mask #= 0xF
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.length #= 4 - 1
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.cmd.ready.toBoolean == true)
     dut.io.bus.cmd.valid #= false
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.rsp.valid.toBoolean == true)
     assert(dut.io.bus.rsp.data.toBigInt == data)
   }
+
+  def readAndCheckWordBurst(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: Seq[BigInt]) = {
+    dut.io.bus.cmd.valid #= true
+    dut.io.bus.cmd.address #= address
+    dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.READ
+    dut.io.bus.cmd.mask #= 0xF
+    dut.io.bus.cmd.length #= (4 * data.length) - 1
+    dut.clockDomain.waitSampling(1)
+    dut.io.bus.cmd.valid #= false
+    for (d <- data) {
+      dut.clockDomain.waitSampling(1)
+      assert(dut.io.bus.rsp.valid.toBoolean == true)
+      assert(dut.io.bus.rsp.data.toBigInt == d)
+    }
+    assert(dut.io.bus.rsp.last.toBoolean == true)
+    assert(dut.io.bus.cmd.ready.toBoolean == true)
+  }
+
 
   def readAndCheckShort(dut: BmbIhpOnChipRam.OnePort1Macro, address: BigInt, data: BigInt) = {
     dut.io.bus.cmd.valid #= true
     dut.io.bus.cmd.address #= address
     dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.READ
     dut.io.bus.cmd.mask #= 0x3
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.length #= 2 - 1
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.cmd.ready.toBoolean == true)
     dut.io.bus.cmd.valid #= false
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.rsp.valid.toBoolean == true)
     assert(dut.io.bus.rsp.data.toBigInt == data)
   }
 
@@ -84,9 +117,12 @@ class BmbOnChipRam1Port1MacroTest extends AnyFunSuite {
     dut.io.bus.cmd.address #= address
     dut.io.bus.cmd.opcode #= Bmb.Cmd.Opcode.READ
     dut.io.bus.cmd.mask #= 0x1
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.cmd.ready.toBoolean)
+    dut.io.bus.cmd.length #= 1 - 1
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.cmd.ready.toBoolean == true)
     dut.io.bus.cmd.valid #= false
-    dut.clockDomain.waitSamplingWhere(dut.io.bus.rsp.valid.toBoolean)
+    dut.clockDomain.waitSampling(1)
+    assert(dut.io.bus.rsp.valid.toBoolean == true)
     assert(dut.io.bus.rsp.data.toBigInt == data)
   }
 
@@ -141,6 +177,20 @@ class BmbOnChipRam1Port1MacroTest extends AnyFunSuite {
       writeChar(dut, 0x0000000, 0x11111111)
       dut.clockDomain.waitSampling(2)
       readAndCheckChar(dut, 0x00000000, 0x00000011)
+
+      dut.clockDomain.waitSampling(20)
+    }
+
+    compiled.doSim("word - burst") { dut =>
+      defaultSignals(dut)
+
+      writeWord(dut, 0x0000000, 0x11111111)
+      writeWord(dut, 0x0000004, 0x22222222)
+      writeWord(dut, 0x0000008, 0x33333333)
+      writeWord(dut, 0x000000c, 0x44444444)
+      dut.clockDomain.waitSampling(2)
+
+      readAndCheckWordBurst(dut, 0x00000000, Seq(0x11111111, 0x22222222, 0x33333333, 0x44444444))
 
       dut.clockDomain.waitSampling(20)
     }
